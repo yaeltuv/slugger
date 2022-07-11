bccb53c74f066e6e189c3a15f1f6dd1861600e46
yaeltuv <yaelt@codefuel.com>
2022-07-11 13:53:06 +0300

    initial commit

---
+++ src/consumer.draft.js
@@ -0,0 +1,5 @@
+import { slugger } from './index.js';
+import marker from '@ajar/marker'; 
+
+
+marker.blue(slugger('hello', 'world'));     
---
+++ src/index.js
@@ -0,0 +1,3 @@
+export function slugger(...args){
+    return args.join('-');
+}
